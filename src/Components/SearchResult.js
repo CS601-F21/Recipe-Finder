7e9d8a69c5d70db51c37facbb33daafe4b195e41
Shubham Pareek <shubhampareek4000@gmail.com>
2021-10-20 15:28:03 -0700

    functional ui is designed

--- src/Components/SearchResult.js
+++ src/Components/SearchResult.js
@@ -34,7 +34,43 @@ const SuggestedRecipes = (props) => {
                     <img className={['foodImage'].join(" ")} src={image}/>
                 </div>
                 <div className={["recipeInfoContainer"].join(" ")}>
-                    {props.recipeName}
+                    <div className={["recipeNameContainer"].join(" ")}>
+                        {props.recipeName}
+                    </div>
+                    <div className={["recipeDetailsContainer"].join(" ")}>
+                        <div className={["recipeTimeTake", "recipeInfo"].join(" ")}>
+                            <div className={["heading"].join(" ")}>
+                                Cooking Time :
+                            </div>
+                            <div className={["timeTaken", "info"].join(" ")}>
+                            {props.timeTaken}
+                            </div>
+                        </div>
+                        <div className={["recipeDifficulty", "recipeInfo"].join(" ")}>
+                            <div className={["heading"].join(" ")}>
+                                Difficulty :
+                            </div>
+                            <div className={["difficulty", "info"].join(" ")}>
+                                {props.difficulty}
+                            </div>
+                        </div>
+                        <div className={["recipeRating", "recipeInfo"].join(" ")}>
+                            <div className={["heading"].join(" ")}>
+                                Rating :
+                            </div>
+                            <div className={["rating", "info"].join(" ")}>
+                                {props.rating}
+                            </div>
+                        </div>
+                        <div className={["recipeTasteProfile", "recipeInfo"].join(" ")}>
+                            <div className={["heading"].join(" ")}>
+                                Taste Profile:
+                            </div>
+                            <div className={["tasteProfile", "info"].join(" ")}>
+                                {props.tasteProfile}
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </li>
@@ -45,10 +81,10 @@ const SearchOutput = (props) => {
     return (
         <div className={["searchResultsWrapper"]}>
             <ul className={["suggestions"].join(" ")}>
-                <SuggestedRecipes recipeName = "Paneer Butter Masala"/>
-                <SuggestedRecipes recipeName = "Paneer Butter Masala"/>
-                <SuggestedRecipes recipeName = "Paneer Butter Masala"/>
-                <SuggestedRecipes recipeName = "Paneer Butter Masala"/>
+                <SuggestedRecipes recipeName = "recipe name" timeTaken = "20mins" difficulty = "3/5" rating = "5/5" tasteProfile = "Sweet" />
+                <SuggestedRecipes recipeName = "recipe name" timeTaken = "20mins" difficulty = "3/5" rating = "5/5" tasteProfile = "Sweet" />
+                <SuggestedRecipes recipeName = "recipe name" timeTaken = "20mins" difficulty = "3/5" rating = "5/5" tasteProfile = "Sweet" />
+                <SuggestedRecipes recipeName = "recipe name" timeTaken = "20mins" difficulty = "3/5" rating = "5/5" tasteProfile = "Sweet" />
             </ul>
         </div>
     )
